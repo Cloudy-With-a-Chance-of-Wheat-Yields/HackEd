3e54fc70137d1480a1bb101398ea1a5f272eda07
Juniper Gardiner <86971433+JunePrimavera@users.noreply.github.com>
2025-02-25 14:51:57 +0000

    Simplify server code

--- server/server.js
+++ server/server.js
@@ -16,8 +16,7 @@ const port = process.env.PORT;
 // Create variables for the data to start with
 var year_one, year_two, year_three, year_four;
 // Create variables related to the raspberry Pi part of the API
-var connected_users = [false, false, false, false, false, false];
-var user_colours = [0, 0, 0, 0, 0, 0];
+var colours = [0, 0, 0, 0, 0, 0];
 
 async function UpdateDatabaseCache() { // Runs indefinitely in the background, updates the cached database values hourly and on first execution
   FetchDatabase();
@@ -59,28 +58,9 @@ app.get("/", async function (request, response) {
   } else {
     // Data for the raspberry pi to connect and disconnect users
     switch(connection) {
-      case "connect": // Provides a user id 
-        var j = 0;
-        connected_users.forEach(function (value, i) {
-          if (!value && j == 0) {
-            connected_users[i] = true;
-            j = i
-          }
-        });
-        console.log("Connection Requested")
-        response.json(j);
-        break;
       case "getc": // Provides a user id
-      console.log("User Colours Requested")
-        response.json(user_colours);   
-        break;
-      default: // Disconnects the user with the id provided in the query
-        var user = Number(request.query["connection"])
-        if (user != NaN) {
-          connected_users[user] = false;
-        }
-        console.log("Disconnect Requested")
-        response.json("")
+        console.log("User Colours Requested")
+        response.json(colours);   
         break;
     }
   }
@@ -90,30 +70,28 @@ app.get("/", async function (request, response) {
 
 // Set colour for a user's LEDs
 app.get("/colour", async function (request, response) {
-  if (Number(request.query["0"]) != NaN) {
-    user_colours[0] = Number(request.query["0"]);
+  console.log(request.query)
+  console.log(request.query[0])
+  console.log(request.query[1])
+  if (!isNaN(Number(request.query["0"])) ) {
+    colours[0] = Number(request.query["0"]);
   }
-  else if (Number(request.query["1"]) != NaN) {
-    user_colours[1] = Number(request.query["1"]);
+  if (!isNaN(Number(request.query["1"]))) {
+    colours[1] = Number(request.query["1"]);
   }
-  else if (Number(request.query["2"]) != NaN) {
-    user_colours[2] = Number(request.query["2"]);
+  if (!isNaN(Number(request.query["2"]))) {
+    colours[2] = Number(request.query["2"]);
   }
-  else if (Number(request.query["3"]) != NaN) {
-    user_colours[3] = Number(request.query["3"]);
+  if (!isNaN(Number(request.query["3"]))) {
+    colours[3] = Number(request.query["3"]);
   }
-  else if (Number(request.query["4"]) != NaN) {
-    user_colours[4] = Number(request.query["4"]);
+  if (!isNaN(Number(request.query["4"]))) {
+    colours[4] = Number(request.query["4"]);
   }
-  else if (Number(request.query["5"]) != NaN) {
-    user_colours[5] = Number(request.query["5"]);
+  if (!isNaN(Number(request.query["5"]))) {
+    colours[5] = Number(request.query["5"]);
   }
-  connected_users.forEach(function (value, i) {
-    if (value == NaN) { // Ensure no NaN values are left over, this can happen if the query argument provided isn't a number
-      connected_users[i] = 0; 
-    }
-  });
-  console.log(user_colours);
+  console.log(colours);
   response.json("Set colour of user's LEDs");
 });
 
@@ -130,16 +108,9 @@ https://hacked-zg4z.onrender.com/?weather=[year]
 Replace square brackets with the year of the data you want to fetch, from 2021 to 2024.
 Returns the weekly average throughout the specified year
 
-Connect/Disconnect a client to the Pi;
-https://hacked-zg4z.onrender.com/?connection=connect
-Connect a new user to the Pi.
-Returns the user ID
-To disconnect a user, replace `connect` with the user ID of the user you want to disconnect
-https://hacked-zg4z.onrender.com/?connection=[userid]
-
 Set/Get the colour for the user's LEDs on the Pi Sense HAT;
-https://hacked-zg4z.onrender.com/colour?[userid]=[colour]
-Replace `userid` with the user's LEDs you want to change the colour of, and `colour` is the value/brightnes of the LED(s)
+https://hacked-zg4z.onrender.com/colour?[id]=[colour]
+Replace `id` with the fields's LEDs you want to change the colour of, and `colour` is the state of the LED
 To get the colours;
 https://hacked-zg4z.onrender.com/?connection=getc
 This will return 6 values, with each user's colour.

b522015b4be03b282739d5ba761599d4dd35bd0b
Juniper Gardiner <juniper.gardiner@gmail.com>
2025-02-25 10:53:35 +0000

    Added work-in-progress api features for the rasperry Pi

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 // Create variables for the data to start with
 var year_one, year_two, year_three, year_four;
+var connected_users = [false, false, false, false, false, false];
+var user_colours = [0, 0, 0, 0, 0, 0];
+
 async function UpdateDatabaseCache() { // Runs indefinitely in the background, updates the cached database values hourly and on first execution
   FetchDatabase();
   while (true) {
@@ -31,6 +34,9 @@ async function FetchDatabase() {
 }
 app.get("/", async function (request, response) { 
   var year = request.query["weather"];
+  var connection = request.query["connection"];
+
+  // Return weather data for each year
   switch(year) {
     case "2021": // Return 1/1/2021 -> 1/1/2022 weather data 
       response.json(year_one.rows);
@@ -43,11 +49,59 @@ app.get("/", async function (request, response) {
       break;
     case "2024": // Return 1/1/2024 -> 1/1/2025 weather data 
       response.json(year_four.rows);
-        break;
-    default:
-      response.json("Successfully reached server!");
+      break;
   } 
+
+  // Data for the raspberry pi to connect and disconnect users
+  switch(connection) {
+    case "connect": // Provides a user id 
+      var j = 0;
+      connected_users.forEach(function (value, i) {
+        if (!value && j == 0) {
+          connected_users[i] = true;
+          j = i
+        }
+      });
+      response.json(j);
+      break;
+    case "getc": // Provides a user id
+      response.json(user_colours);   
+      break;
+    default: // Disconnects the user with the id provided in the query
+      connected_users[request.query["connection"]] = false;
+      response.json(request.query["connection"]);
+      break;
+  }
 });
+
+app.get("/colour", async function (request, response) {
+  if (Number(request.query["0"]) != NaN) {
+    user_colours[0] = Number(request.query["0"]);
+  }
+  else if (Number(request.query["1"]) != NaN) {
+    user_colours[1] = Number(request.query["1"]);
+  }
+  else if (Number(request.query["2"]) != NaN) {
+    user_colours[2] = Number(request.query["2"]);
+  }
+  else if (Number(request.query["3"]) != NaN) {
+    user_colours[3] = Number(request.query["3"]);
+  }
+  else if (Number(request.query["4"]) != NaN) {
+    user_colours[4] = Number(request.query["4"]);
+  }
+  else if (Number(request.query["5"]) != NaN) {
+    user_colours[5] = Number(request.query["5"]);
+  }
+  connected_users.forEach(function (value, i) {
+    if (value == NaN) {
+      connected_users[i] = old_colours[i];
+    }
+  });
+  console.log(user_colours);
+  response.json("Set colour of user's LEDs");
+});
+
 
 // start the server
 app.listen(8080, () => console.log("API Server is running on port 8080"));

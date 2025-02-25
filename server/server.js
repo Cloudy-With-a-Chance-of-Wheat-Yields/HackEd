00864c1297e400224b82d86bc57b29730b1f51ec
Juniper Gardiner <juniper.gardiner@gmail.com>
2025-02-25 11:02:42 +0000

    Added documentation

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 // Create variables for the data to start with
 var year_one, year_two, year_three, year_four;
+// Create variables related to the raspberry Pi part of the API
 var connected_users = [false, false, false, false, false, false];
 var user_colours = [0, 0, 0, 0, 0, 0];
 
@@ -24,7 +25,7 @@ async function UpdateDatabaseCache() { // Runs indefinitely in the background, u
     await new Promise(resolve => setTimeout(resolve, 600000));  
     FetchDatabase();
     console.log("Updated Database Cache!")
-  }
+  } 
 }
 async function FetchDatabase() {
   year_one = await db.query("SELECT * FROM weather_2021",);
@@ -74,6 +75,7 @@ app.get("/", async function (request, response) {
   }
 });
 
+// Set colour for a user's LEDs
 app.get("/colour", async function (request, response) {
   if (Number(request.query["0"]) != NaN) {
     user_colours[0] = Number(request.query["0"]);
@@ -94,8 +96,8 @@ app.get("/colour", async function (request, response) {
     user_colours[5] = Number(request.query["5"]);
   }
   connected_users.forEach(function (value, i) {
-    if (value == NaN) {
-      connected_users[i] = old_colours[i];
+    if (value == NaN) { // Ensure no NaN values are left over, this can happen if the query argument provided isn't a number
+      connected_users[i] = 0; 
     }
   });
   console.log(user_colours);
@@ -106,3 +108,26 @@ app.get("/colour", async function (request, response) {
 // start the server
 app.listen(8080, () => console.log("API Server is running on port 8080"));
 UpdateDatabaseCache();
+
+/*
+Brief API Documentation;
+
+Fetch weather data
+https://hacked-zg4z.onrender.com/?weather=[year]
+Replace square brackets with the year of the data you want to fetch, from 2021 to 2024.
+Returns the weekly average throughout the specified year
+
+Connect/Disconnect a client to the Pi;
+https://hacked-zg4z.onrender.com/?connection=connect
+Connect a new user to the Pi.
+Returns the user ID
+To disconnect a user, replace `connect` with the user ID of the user you want to disconnect
+https://hacked-zg4z.onrender.com/?connection=[userid]
+
+Set/Get the colour for the user's LEDs on the Pi Sense HAT;
+https://hacked-zg4z.onrender.com/colour?[userid]=[colour]
+Replace `userid` with the user's LEDs you want to change the colour of, and `colour` is the value/brightnes of the LED(s)
+To get the colours;
+https://hacked-zg4z.onrender.com/?connection=getc
+This will return 6 values, with each user's colour.
+*/

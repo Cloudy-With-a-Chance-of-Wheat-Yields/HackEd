fa6c036bf499b6e632c3cd3b9711b78fbd8b5b8a
Juniper Gardiner <juniper.gardiner@gmail.com>
2025-02-25 09:57:05 +0000

    Update API server to use http GET queries

--- server/server.js
+++ server/server.js
@@ -13,37 +13,40 @@ dotenv.config();
 // set up our database connection
 const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-// create my test endpoint
-app.get("/", (request, response) => response.json("Its working"));
-
-// Request the data for each year from the database
-var year_one = await db.query("SELECT * FROM weather_2021",);
-var year_two = await db.query("SELECT * FROM weather_2022",);
-var year_three = await db.query("SELECT * FROM weather_2023",);
-var year_four = await db.query("SELECT * FROM weather_2024",);
-async function UpdateDatabaseCache() { // Runs indefinitely in the background, updates the cached database values hourly
+// Create variables for the data to start with
+var year_one, year_two, year_three, year_four;
+async function UpdateDatabaseCache() { // Runs indefinitely in the background, updates the cached database values hourly and on first execution
+  FetchDatabase();
   while (true) {
     await new Promise(resolve => setTimeout(resolve, 600000));  
-    year_one = await db.query("SELECT * FROM weather_2021",);
-    year_two = await db.query("SELECT * FROM weather_2022",);
-    year_three = await db.query("SELECT * FROM weather_2023",);
-    year_four = await db.query("SELECT * FROM weather_2024",);
+    FetchDatabase();
     console.log("Updated Database Cache!")
   }
 }
-
-app.get("/2021", async function (request, response) { // Return 1/1/2021 -> 1/1/2022 weather data 
-  response.json(year_one.rows);
-});
-
-app.get("/2022", async function (request, response) { // Return 1/1/2022 -> 1/1/2023 weather data 
-  response.json(year_two.rows);
-});
-app.get("/2023", async function (request, response) { // Return 1/1/2023 -> 1/1/2024 weather data 
-  response.json(year_three.rows);
-});
-app.get("/2024", async function (request, response) { // Return 1/1/2024 -> 1/1/2025 weather data 
-  response.json(year_four.rows);
+async function FetchDatabase() {
+  year_one = await db.query("SELECT * FROM weather_2021",);
+  year_two = await db.query("SELECT * FROM weather_2022",);
+  year_three = await db.query("SELECT * FROM weather_2023",);
+  year_four = await db.query("SELECT * FROM weather_2024",);
+}
+app.get("/", async function (request, response) { 
+  var year = request.query["weather"];
+  switch(year) {
+    case "2021": // Return 1/1/2021 -> 1/1/2022 weather data 
+      response.json(year_one.rows);
+      break;
+    case "2022": // Return 1/1/2022 -> 1/1/2023 weather data 
+      response.json(year_two.rows);
+      break;
+    case "2023": // Return 1/1/2023 -> 1/1/2024 weather data 
+      response.json(year_three.rows);
+      break;
+    case "2024": // Return 1/1/2024 -> 1/1/2025 weather data 
+      response.json(year_four.rows);
+        break;
+    default:
+      response.json("Successfully reached server!");
+  } 
 });
 
 // start the server
